cd73f65a790577b8580d3a6f8ae402b5b153ffa1
anonymousone1895 <38712538+phaniraghava1234@users.noreply.github.com>
2026-02-26 17:13:41 +0100

    Update CV link in config.js

--- config.js
+++ config.js
@@ -2,4 +2,4 @@
 //  To update your CV link: paste your Google Drive URL below
 // ============================================================
 
-const CV_LINK = "https://drive.google.com/YOUR_LINK_HERE";
+const CV_LINK = "https://drive.google.com/file/d/17FsSRVUnW1TRC-oWhodRmXvMfdiGygJQ/view?usp=sharing";
