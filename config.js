17c6248180d44923f743e2185f8e339527d3caa7
anonymousone1895 <38712538+phaniraghava1234@users.noreply.github.com>
2026-02-26 17:13:21 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// ============================================================
+//  To update your CV link: paste your Google Drive URL below
+// ============================================================
+
+const CV_LINK = "https://drive.google.com/YOUR_LINK_HERE";
